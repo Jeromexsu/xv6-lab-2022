4e934c3816389580f04c2eebe83c564d69d9862f
Jerome Su <suchuanxj@gmail.com>
2023-11-23 20:16:39 +0800

    [syscall lab] 2.1 syscall hooked 😆

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -128,6 +128,36 @@ static uint64 (*syscalls[])(void) = {
 [SYS_close]   sys_close,
 };
 
+static char *syscallNames[]= {
+[SYS_fork]    "sys_fork",
+[SYS_exit]    "sys_exit",
+[SYS_wait]    "sys_wait",
+[SYS_pipe]    "sys_pipe",
+[SYS_read]    "sys_read",
+[SYS_kill]    "sys_kill",
+[SYS_exec]    "sys_exec",
+[SYS_fstat]   "sys_fstat",
+[SYS_chdir]   "sys_chdir",
+[SYS_dup]     "sys_dup",
+[SYS_getpid]  "sys_getpid",
+[SYS_sbrk]    "sys_sbrk",
+[SYS_sleep]   "sys_sleep",
+[SYS_uptime]  "sys_uptime",
+[SYS_open]    "sys_open",
+[SYS_write]   "sys_write",
+[SYS_mknod]   "sys_mknod",
+[SYS_unlink]  "sys_unlink",
+[SYS_link]    "sys_link",
+[SYS_mkdir]   "sys_mkdir",
+[SYS_close]   "sys_close",
+#ifdef LAB_NET
+[SYS_connect] "sys_connect",
+#endif
+#ifdef LAB_PGTBL
+[SYS_pgaccess] "sys_pgaccess",
+#endif
+};
+
 void
 syscall(void)
 {
@@ -139,6 +169,8 @@ syscall(void)
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
     p->trapframe->a0 = syscalls[num]();
+    printf("%d: syscall %s -> %d\n",p->pid,syscallNames[num],p->trapframe->a0);
+
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
